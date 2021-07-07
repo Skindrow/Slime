cce2c000d19ca57bd7a710cf512df499177c3261
Skindrow <75908610+Skindrow@users.noreply.github.com>
2021-07-07 11:07:32 +0300

    LoseScreen

--- Assets/Scripts/Spawners/Spawner.cs
+++ Assets/Scripts/Spawners/Spawner.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Spawner : MonoBehaviour
 {
@@ -8,11 +9,20 @@ public class Spawner : MonoBehaviour
     [SerializeField] private float spawnBoundX;
     [SerializeField] private float spawnBoundY;
     [SerializeField] private GameObject[] spawnedGO;
+    [SerializeField] private RectTransform canvasRectTransform;
+    [SerializeField] private Transform canvasTransform;
 
+    private void Awake()
+    {
+
+    }
     private void Start()
     {
+        spawnBoundX = canvasRectTransform.rect.width - canvasRectTransform.rect.width / 10;
+        spawnBoundY = canvasRectTransform.rect.height - canvasRectTransform.rect.height / 10;
         StartCoroutine(SpawnBursts());
     }
+
 
 
 
@@ -22,13 +32,16 @@ public class Spawner : MonoBehaviour
         {
             int rnd = Random.Range(0, spawnedGO.Length);
 
-            float rndX = Random.Range(-spawnBoundX, spawnBoundX);
-            float rndY = Random.Range(-spawnBoundY, spawnBoundY);
+            GameObject burstGO = Instantiate(spawnedGO[rnd].gameObject, canvasTransform);
 
-            GameObject burstGO = Instantiate(spawnedGO[rnd].gameObject, new Vector2(rndX, rndY), Quaternion.identity);
+
+            float rndX = Random.Range(-spawnBoundX / 2, spawnBoundX / 2);
+            float rndY = Random.Range(-spawnBoundY / 2, spawnBoundY / 2);
+
+            burstGO.transform.localPosition = new Vector3(rndX, rndY, 0);
 
             yield return new WaitForSeconds(timeToSpawn);
         }
     }
-    
+
 }
